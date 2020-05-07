6fa63980f85a2613368aaea3c0b084fb6b0e1024
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-05-07 21:01:04 +0100

    Added 2 screens

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Container(
+          padding: const EdgeInsets.all(8),
+          child: Text("This works!"),
+        ),
+      ),
+    );
+  }
+}
