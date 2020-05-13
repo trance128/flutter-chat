e3c598d96450087f57b384b7ba71228da156d35b
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-05-13 20:07:30 +0100

    users can now choose profile pic

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
+import '../pickers/user_image_picker.dart';
 
 class AuthForm extends StatefulWidget {
   AuthForm(this.submitFn, this.isLoading);
@@ -7,6 +11,7 @@ class AuthForm extends StatefulWidget {
     String email,
     String userName,
     String password,
+    File userImageFile,
     bool isLogin,
     BuildContext ctx,
   ) submitFn;
@@ -22,10 +27,25 @@ class _AuthFormState extends State<AuthForm> {
   String _userEmail = '';
   String _userName = '';
   String _userPassword = '';
+  File _userImageFile;
+
+  void _pickedImage(File image) {
+    _userImageFile = image;
+  }
 
   void _trySubmit() {
     final isValid = _formKey.currentState.validate();
     FocusScope.of(context).unfocus();
+
+    if (_userImageFile == null && !_isLogin) {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please pick an image'),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+      return;
+    }
 
     if (isValid) {
       _formKey.currentState.save();
@@ -33,6 +53,7 @@ class _AuthFormState extends State<AuthForm> {
         _userEmail.trim(),
         _userName.trim(),
         _userPassword.trim(),
+        _userImageFile,
         _isLogin,
         context,
       );
@@ -52,6 +73,7 @@ class _AuthFormState extends State<AuthForm> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
+                  if (!_isLogin) UserImagePicker(_pickedImage),
                   TextFormField(
                     key: ValueKey('email'),
                     validator: (value) {
---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  final void Function(File pickedImage) imagePickFn;
+
+  UserImagePicker(this.imagePickFn);
+
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+
+  void _pickImage() async {
+    final pickedImageFile =
+        await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _pickedImage = pickedImageFile;
+    });
+    widget.imagePickFn(pickedImageFile);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 40,
+          backgroundImage: _pickedImage != null ? FileImage(_pickedImage) : null,
+        ),
+        FlatButton.icon(
+          textColor: Theme.of(context).primaryColor,
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+        ),
+      ],
+    );
+  }
+}
